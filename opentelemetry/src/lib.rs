0e7058fe4f7a10c59b2afcb6aee98af6be54fe9c
Or Ricon <rikonor@gmail.com>
2024-04-26 17:04:09 -0400

    remove js-sys to avoid wasm issues

--- opentelemetry/src/lib.rs
+++ opentelemetry/src/lib.rs
@@ -247,17 +247,8 @@ pub mod time {
     use std::time::SystemTime;
 
     #[doc(hidden)]
-    #[cfg(any(
-        not(target_arch = "wasm32"),
-        all(target_arch = "wasm32", target_os = "wasi")
-    ))]
     pub fn now() -> SystemTime {
-        SystemTime::now()
-    }
-
-    #[doc(hidden)]
-    #[cfg(all(target_arch = "wasm32", not(target_os = "wasi")))]
-    pub fn now() -> SystemTime {
-        SystemTime::UNIX_EPOCH + std::time::Duration::from_millis(js_sys::Date::now() as u64)
+        // TODO(rikonor): Use hard-coded time
+        SystemTime::UNIX_EPOCH
     }
 }
